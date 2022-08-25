d62a68625f7ce7c1b0f418f276bab8bf7ab5a6bd
Vndark5464 <nguyenhuulinh5464@gmail.com>
2022-08-25 09:35:50 +0700

    t2204m-nodejs

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT | 5000;
+app.listen(port,function (){
+    console.log("Server is running ...");
+})
+app.get("/",function (req,res){
+    res.send("Hello World !");
+})
+app.get("/bong-da",function (req,res) {
+    res.send("Bóng Đá 24h")
+})
